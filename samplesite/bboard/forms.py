96363cf98fe98264fa367e3095098e41eaa1819b
Bezusov <bezusovalex@gmail.com>
2023-10-24 23:33:42 +0300

    add value None in BBForms

--- samplesite/bboard/forms.py
+++ samplesite/bboard/forms.py
@@ -1,9 +1,8 @@
 from django.forms import ModelForm
-
 from .models import BB
 
 
 class BbForm(ModelForm):
     class Meta:
         model = BB
-        fields = ('title', 'content', 'price', 'rubric')
+        fields = ('title', 'content', 'price', 'rubric', 'kind')
--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -3,13 +3,10 @@ from django.db import models
 
 class BB(models.Model):
     KINDS = [
-        ['Купля-продажа', [
-             ['b', 'Куплю'],
-             ['s', 'Продам'],
-            ]],
-        ['Обмен', [
-            ['c', 'Обменяю'],
-        ]]
+        [None, 'Выберите тип публикуемого объявления'],
+        ['b', 'Куплю'],
+        ['s', 'Продам'],
+        ['c', 'Обменяю'],
     ]
 
     title = models.CharField(max_length=50, verbose_name='Товар')
@@ -17,7 +14,7 @@ class BB(models.Model):
     price = models.FloatField(null=True, blank=True, verbose_name='Цена')
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')
     rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Рубрика')
-    kind = models.CharField(max_length=1, choices=KINDS, default='s', verbose_name='Тип')
+    kind = models.CharField(max_length=1, choices=KINDS, blank=True, verbose_name='Тип')
 
     class Meta:
         verbose_name_plural = 'Объявления'
